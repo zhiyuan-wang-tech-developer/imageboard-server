bcc4eac96c06b15f43f7bb7e8982207064c28eb0
Zhiyuan Wang <zhiyuan.wang.hightech@gmail.com>
2020-05-17 20:57:12 +0200

    add POST /image endpoint. 20200517 20:56

--- image/router.js
+++ image/router.js
@@ -7,6 +7,17 @@ const imageModel = require('./model')
 // Instantiate a new router
 const router = new Router()
 
+// POST request to create a new image post in the database
+router.post('/image', (request, response, next) => {
+    imageModel
+        .create(request.body)
+        .then(image => {
+            // send back the new image post you created
+            response.send(image)
+        })
+        .catch(next)
+})
+
 // GET request to read all images
 router.get('/image', (request, response, next) => {
     imageModel
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Import express
 const express = require('express')
+// Import CORS
+const cors = require('cors')
+// Import body parser
+const bodyParser = require('body-parser')
 // Import database
 const database = require('./database')
 const imageModel = require('./image/model')
@@ -7,6 +11,17 @@ const imageRouter = require('./image/router')
 
 // Create a new express server
 const app = express()
+
+// Enable Cross-Origin-Resource-Sharing
+// cors() returns a middleware
+// cors() needs to add the header 'Access-Control-Allow-Origin' first
+const corsMiddleware = cors()
+// The order matters: make sure you register cors() before all other middlewares and routes
+// Add header field 'Access-Control-Allow-Origin: *'
+app.use(corsMiddleware)
+
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
 
 // Server port
 const port = process.env.PORT || 4000
